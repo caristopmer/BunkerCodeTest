d6cc97fbddd3a080210086e35f5852961639d1e4
caristopmer <chris.mcmenomy@gmail.com>
2017-12-04 10:20:57 -0600

    Color Form component added to project and working. Also added to navigation bar and is functional.

--- bunker-code-test/src/app/routing/routing.module.ts
+++ bunker-code-test/src/app/routing/routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from "app/home/home.component";
 import { InstructionsComponent } from "app/instructions/instructions.component";
+import { ColorFormComponent } from "app/color-form/color-form.component";
 
 const routes: Routes = [
     { 'path': '', 'redirectTo': 'Home', 'pathMatch': 'full' },
     { 'path': 'Home', 'component': HomeComponent },
-    { 'path': 'Instructions', 'component': InstructionsComponent }
+    { 'path': 'Instructions', 'component': InstructionsComponent },
+    { 'path': 'Color Form', 'component': ColorFormComponent }
 ];
 
 @NgModule({
