b3d4fd40a12a3d95fbeaabfd6f81d01450e85c46
caristopmer <chris.mcmenomy@gmail.com>
2017-12-04 10:32:49 -0600

    Basic favorite color form created and laid out.

--- bunker-code-test/src/app/routing/routing.module.ts
+++ bunker-code-test/src/app/routing/routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from "app/home/home.component";
-import { InstructionsComponent } from "app/instructions/instructions.component";
-import { ColorFormComponent } from "app/color-form/color-form.component";
+import { HomeComponent } from 'app/home/home.component';
+import { InstructionsComponent } from 'app/instructions/instructions.component';
+import { ColorFormComponent } from 'app/color-form/color-form.component';
 
 const routes: Routes = [
     { 'path': '', 'redirectTo': 'Home', 'pathMatch': 'full' },
